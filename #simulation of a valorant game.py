0ee0c439dd3b7726b2b7dee49bc5b6acbe1f7e77
Pineapple840 <Pineapple840@protonmail.com>
2022-02-16 16:41:34 +0000

    initial commit

---
+++ #simulation of a valorant game.py
@@ -0,0 +1,22 @@
+#simulation of a valorant game
+import random
+
+#players
+
+player1 = "Dream"
+player2 = "Obama"
+player3 = "Amogus",
+player4 = "I'm running out of ideas",
+player5 = "John Xina",
+player6 = "Sans",
+player7 = "Shrek",
+player8 = "Cringe",
+player9 = "Keanu Reeves",
+player10 = "Jesus",
+
+
+playerdict = [
+player1,player2,player3,player4,player5,player6,player7,player8,p9,player10
+]
+
+valorantagents = []
